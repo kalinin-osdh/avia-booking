8092387f12b5447b255965091b0106c7eb5effd7
Вадим <kalinin.osdh@gmail.com>
2026-07-19 16:34:53 +0300

    admin cache created

--- flight-service/src/main/java/ru/kalinin/flight/config/RedisConfig.java
+++ flight-service/src/main/java/ru/kalinin/flight/config/RedisConfig.java
@@ -1,26 +1,29 @@
-package ru.kalinin.common.config;
+package ru.kalinin.flight.config;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+import ru.kalinin.flight.entity.Flight;
 
 @Configuration
 @ConditionalOnClass(RedisConnectionFactory.class)
 public class RedisConfig {
 
-/*    @Bean
-    public RedisTemplate<String, String> customStringRedisTemplate(RedisConnectionFactory connectionFactory) {
-        RedisTemplate<String, String> template = new RedisTemplate<>();
+    @Bean
+    public RedisTemplate<String , Flight> flightRedisTemplate(RedisConnectionFactory connectionFactory) {
+        RedisTemplate<String, Flight> template = new RedisTemplate<>();
 
         template.setConnectionFactory(connectionFactory);
 
         template.setKeySerializer(new StringRedisSerializer());
-        template.setValueSerializer(new StringRedisSerializer());
+        template.setValueSerializer(new GenericJackson2JsonRedisSerializer());
 
         return template;
-    }*/
+    }
 
 }
